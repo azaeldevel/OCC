0a3e474083c96cb8473744aa5fa2ba57f1a4d8e1
Azael <azael.devel@gmail.com>
2023-02-15 21:21:28 -0800

    save

--- src/develop-lexer-c90.cc
+++ src/develop-lexer-c90.cc
@@ -237,8 +237,8 @@ constexpr size_t initial_lex_c_90 = 0;
 constexpr size_t max_status_c_90 = 4;
 
 const std::vector<char> digits {'0','1','2','3','4','5','6','7','8','9'};
-const std::vector<char> lower = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'};
-const std::vector<char> upper = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','Z'};
+const std::vector<char> lower = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+const std::vector<char> upper = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 const std::vector<char> graphic = { '!','"','#','%','%','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','[','\\',']','^','_','{','|','}','~'};
 const std::vector<char> display = { '\a','\b','\f','\n','\r','\t','\v'};
 const std::vector<char> not_c = {' '};
@@ -344,10 +344,7 @@ constexpr LexC90 create_tt_c90()
 	{
 		c90.word(p.string,p.token, symbols_end_words);
 	}
-	if (c90.almost_one(digits, Tokens_C90::integer, symbols_end_words) == LexC90::USED)
-	{
-
-	}
+	c90.almost_one(digits, Tokens_C90::integer, symbols_end_words);
 	
 
 	return c90;
