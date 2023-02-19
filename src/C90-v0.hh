b62700ef21da092623337680a35165273e120553
Azael <azael.devel@gmail.com>
2023-02-19 11:35:16 -0800

    save

--- src/C90-v0.hh
+++ src/C90-v0.hh
@@ -401,7 +401,8 @@ namespace oct::cc::v0::c90
 				symbols_identifier.push_back(c);
 			}
 			symbols_identifier.push_back('_');
-			some(symbols_identifier, Tokens::identifier, symbols_end_words,core_here::lex::Flag::join_same);
+			one(symbols_identifier, Tokens::identifier, symbols_end_words,core_here::lex::Flag::only_free);
+			//some(symbols_identifier, Tokens::identifier, symbols_end_words,core_here::lex::Flag::);
 		}
 
 	private:
