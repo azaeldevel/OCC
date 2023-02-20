13179114a690bc5806d4fe4371618b1e3b1ab647
Azael Reyes <azael.devel@gmail.com>
2023-02-20 14:43:43 -0800

    save

---
+++ src/develop-lexer-flex-c90.c
@@ -0,0 +1,27 @@
+
+/*
+ * develop-lexer-c90.cc
+ * Copyright (C) 2022 Azael Reyes <azael.devel@gmail.com>
+ *
+ * Octetos Compuler Collection is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CC is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+int main() 
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
