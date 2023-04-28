2f59acbbc89a49e9250cd81ffc17d1134302d74f
Azael Reyes <azael.devel@gmail.com>
2023-04-28 00:46:36 -0700

    save

--- 0/oct-core-v3.hh
+++ 0/oct-core-v3.hh
@@ -73,25 +73,6 @@ namespace oct::core::v3
             //std::cout << "Block::create end\n";
             return new(now) T;
         }
-        void* create(std::size_t s)
-        {
-            //std::cout << "Block::create begin\n";
-            if((long)actual >= (long)page_size)
-            {
-                //std::cout << "Block::create malloc\n";
-                actual = malloc(page_size);
-                blocks.push_back(actual);
-            }
-            void* now = actual;
-            char* newptr = reinterpret_cast<char*>(actual);
-            //std::cout << "Block::create actual : " << (long)actual << "\n";
-            newptr += s + 1;
-            //std::cout << "Block::create newptr : " << (long)newptr << "\n";
-            //std::cout << "Block::create sizeof(T) : " << sizeof(T) << "\n";
-            actual = (void*)newptr;
-            //std::cout << "Block::create end\n";
-            return now;
-        }
     protected:
         size_t page_size;
         void* actual;//last block memory assignable
